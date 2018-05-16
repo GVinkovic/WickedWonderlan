002108c5d7b3fa4b44a416938d7467ac8641bc4e
GVinkovic <38727804+GVinkovic@users.noreply.github.com>
2018-05-16 22:02:47 +0200

    Ubacivanje Intro scene između MainMenu i World scene

---
+++ Assets/Intro/LoadNewScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewScene : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void loadWorld(string World)
+    {
+        SceneManager.LoadScene(World);
+    }
+}
