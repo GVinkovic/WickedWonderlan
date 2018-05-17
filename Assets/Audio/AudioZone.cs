3d3849bdb38df125a40f5ce4fe949ee0a816e037
GVinkovic <38727804+GVinkovic@users.noreply.github.com>
2018-05-17 23:04:13 +0200

    Dodao Audio zonu na selo i soundtrack za selo.

---
+++ Assets/Audio/AudioZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioZone : MonoBehaviour
+{
+    
+    private string theCollider;
+
+    void OnTriggerEnter(Collider other)
+    {
+        theCollider = other.tag;
+        if (theCollider == "Player")
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+            audio.loop = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        theCollider = other.tag;
+        if (theCollider == "Player")
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Stop();
+        }
+    }
+}
